5aed11926e9856170ee03da7bf2114b4a46c7e6f
zksir <zksir@qq.com>
2020-05-28 09:35:28 +0800

    可重入粒度测试：递归调用本方法

--- src/com/zksir/synchroized/SynchronizedRecursion.java
+++ src/com/zksir/synchroized/SynchronizedRecursion.java
@@ -1,4 +1,21 @@
 package com.zksir.synchroized;
 
+/**
+ * 可重入粒度测试：递归调用本方法
+ */
 public class SynchronizedRecursion {
+    int a = 0;
+
+    public synchronized void method1() {
+        System.out.println("这是method1,a = " + a);
+        if (a == 0) {
+            a++;
+            method1();
+        }
+    }
+
+    public static void main(String[] args) {
+        SynchronizedRecursion synchronizedRecursion = new SynchronizedRecursion();
+
+    }
 }
