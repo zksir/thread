df63bdce2c04c4383dc3d1e7aa8a217b696c010c
zksir <zksir@qq.com>
2020-05-28 17:50:51 +0800

    反编译字节码

--- src/com/zksir/synchroized/Decompilation.java
+++ src/com/zksir/synchroized/Decompilation.java
@@ -2,6 +2,8 @@ package com.zksir.synchroized;
 
 /**
  * 反编译字节码
+ *  1、编译javac Decompilation
+ *  2、反编译（反汇编）javap -verbose(具体的) Decompilation.class
  */
 public class Decompilation {
     private Object object = new Object();
